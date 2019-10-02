603b58ca67dc858ceb1321dda249913c3e397370
Elena Ivanova <iea140777@gmail.com>
2019-10-02 11:41:28 +0300

    add bullet_list and filter_date_dropdown (ui kit form elements)

---
+++ src/includes/filter_date_dropdown/filter_date_dropdown.js
@@ -0,0 +1,10 @@
+$('#date_range').datepicker({
+    clearButton: true,
+    autoClose: false,
+    inline: false,
+    range: true,
+    multipleDatesSeparator: " - ",
+    multipleDates: 2,
+    prevHtml: '<i class="material-icons arrow_back">arrow_back</i>',
+    nextHtml: '<i class="material-icons arrow_forward">arrow_forward</i>',
+})
