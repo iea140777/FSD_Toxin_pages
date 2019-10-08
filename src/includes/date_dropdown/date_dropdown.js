bb94307c561459a1d048e7501a19155d1d871b9f
Elena Ivanova <iea140777@gmail.com>
2019-10-08 11:01:15 +0300

    corrected datepicker & get rig of blue borders (ui kir form elements)

--- src/includes/date_dropdown/date_dropdown.js
+++ src/includes/date_dropdown/date_dropdown.js
@@ -3,19 +3,40 @@ $('#start_one').datepicker({
     onSelect: function (fd, d, picker) { 
       $("#start_one").val(fd.split("-")[0]);
       $("#end_one").val(fd.split("-")[1]);
-      
     },
     clearButton: true,
     autoClose: false,
+    position: "right bottom",
+    offset: 0,
     inline: false,
     prevHtml: '<i class="material-icons arrow_back">arrow_back</i>',
     nextHtml: '<i class="material-icons arrow_forward">arrow_forward</i>',
-       
-  });
- 
-  $("#start_one").click(function(){
-        $(".open_icon").toggleClass("close_icon");
+    position: 'bottom left',
+    navTitles: {
+      days: 'MM <i>yyyy</i>',
+      months: 'yyyy',
+      years: 'yyyy1 - yyyy2'
+  }, 
 });
+  
 
 $( '<i class="material-icons date_dropdown_arrow">expand_more</i>' ).insertAfter( ".text-field__form_narrow" );
+
+$('.datepicker--buttons').append('<span class="datepicker--button -apply">Применить</span>');
  
+
+
+    // var template = '' +
+    //     '<div class="datepicker--nav-action" data-action="prev">#{prevHtml}</div>' +
+    //     '<div class="datepicker--nav-title">#{title}</div>' +
+    //     '<div class="datepicker--nav-action" data-action="next">#{nextHtml}</div>',
+    //     buttonsContainerTemplate = '<div class="datepicker--buttons"></div>',
+    //     button = '<span class="datepicker--button" data-action="#{action}">#{label}</span>' +
+    //     button = '<span class="datepicker--button -apply">Применить</span>',
+    //     datepicker = $.fn.datepicker,
+    //     dp = datepicker.Constructor,
+
+ 
+  //   $("#start_one").click(function(){
+  //         $(".date_dropdown_arrow").toggleClass("close_icon");
+  // });
--- src/includes/filter_date_dropdown/filter_date_dropdown.js
+++ src/includes/filter_date_dropdown/filter_date_dropdown.js
@@ -1,3 +1,4 @@
+
 $('#date_range').datepicker({
     clearButton: true,
     autoClose: false,
@@ -7,6 +8,14 @@ $('#date_range').datepicker({
     multipleDates: 2,
     prevHtml: '<i class="material-icons arrow_back">arrow_back</i>',
     nextHtml: '<i class="material-icons arrow_forward">arrow_forward</i>',
+    position: 'bottom left',
+    navTitles: {
+      days: 'MM <i>yyyy</i>',
+      months: 'yyyy',
+      years: 'yyyy1 - yyyy2'
+    }
 });
 
-$( '<i class="material-icons filter_date_dropdown_arrow">expand_more</i>' ).insertAfter( ".filter_date_dropdow_input" );
+$( '<i class="material-icons filter_date_dropdown_arrow">expand_more</i>' ).insertAfter( ".filter_date_dropdown_input" );
+
+$('.datepicker--buttons').append('<span class="datepicker--button -apply">Применить</span>');
