29d008cf26b41031045904b6e90bd1c3468e35d2
Elena Ivanova <iea140777@gmail.com>
2019-12-09 17:20:47 +0300

    added block header(ui kit)

---
+++ src/blocks/header/header.js
@@ -0,0 +1,4 @@
+$(".header__header-menu__list-item:nth-of-type(3)").click(function(){
+    $(".header__header-menu__services").toggle();
+    $(".header-menu__open-icon").toggleClass("header-menu__close-icon");
+});
