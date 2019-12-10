29be2d9c7a4a68f153c9752167a12e775b104b35
Elena Ivanova <iea140777@gmail.com>
2019-12-10 14:05:38 +0300

    added footer (ui kit) not finished

--- src/blocks/textfield-subscription/textfield-subscription.js
+++ src/blocks/textfield-subscription/textfield-subscription.js
@@ -1,1 +1,1 @@
-$( '<button class="textfield-subscription__button", type="button", formaction="#"><i class="material-icons textfield-subscription__arrow-forward">arrow_forward</i></button>' ).insertAfter( ".textfield-subscription__input" );
+$( '<button class="textfield-subscription__button", type="button", formaction="#"><i class="material-icons textfield-subscription__arrow-forward">arrow_forward</i></button>' ).insertAfter(".textfield-subscription");
