58fea0c1550873ef49e706b11a491d55e5f3723d
Elena Ivanova <iea140777@gmail.com>
2020-01-20 17:45:36 +0300

    added dist folder for test publishing on ghPages

--- src/mixins/button-mixin/button-mixin.js
+++ src/mixins/button-mixin/button-mixin.js
@@ -1,12 +1,11 @@
-$('<i class="material-icons button__arrow-forward">arrow_forward</i>' ).insertAfter( ".button_arrow" );
 
-$('.button_bordered').hover(
-    function(){
-      $(this).parent().css("opacity", "0.5");
-    },
-    function(){
-        $(this).parent().css("opacity", "1");
-      }
-)
+// $('.button_bordered').hover(
+//     function(){
+//       $(this).parent().css("opacity", "0.5");
+//     },
+//     function(){
+//         $(this).parent().css("opacity", "1");
+//       }
+// )
 
 
