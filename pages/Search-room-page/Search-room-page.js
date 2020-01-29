c6efd27c4c539c7d6472a035b3f69555a9318c49
Elena Ivanova <iea140777@gmail.com>
2020-01-29 11:33:21 +0300

    added responsive to Search room page

---
+++ pages/Search-room-page/Search-room-page.js
@@ -0,0 +1,5 @@
+$(".search-room__filter-container-header-mobile").click(function(){
+    $(".search-room__filter__container").toggleClass('search-room__filter__container_open');
+    $(".search-room__menu-open-button").toggleClass('search-room__menu-close-button');
+});
+
